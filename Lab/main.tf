b94d002b8a1e5fdc758fddff0afba004b6948bfb
KristianFJones <me@kristianjones.dev>
2021-09-29 04:50:34 -0500

    feat: Attempt to create thing

--- Lab/main.tf
+++ Lab/main.tf
@@ -9,6 +9,11 @@ terraform {
       source = "hashicorp/vault"
       version = "2.24.0"
     }
+
+    time = {
+      source = "hashicorp/time"
+      version = "0.7.2"
+    }
   }
 }
 
@@ -91,4 +96,149 @@ resource "vault_pki_secret_backend_root_cert" "RootCA" {
   # Vault Docs: https://www.vaultproject.io/api/secret/pki#key_bits-2
   #
   key_bits = 384
+}
+
+#
+# Wait for 30 seconds after creating root
+#
+
+resource "time_sleep" "RootPKIWait" {
+  depends_on = [vault_pki_secret_backend_root_cert.RootCA]
+
+  create_duration = "30s"
+}
+
+#
+# Randomly generate the INT Mount string
+#
+
+resource "random_string" "Mount" {
+  length           = 10
+
+  special = false
+  upper = true
+}
+
+resource "vault_mount" "PKI" {
+  depends_on = [
+    time_sleep.RootPKIWait
+  ]
+
+  path        = random_string.Mount.result
+
+  type        = "pki"
+  description = "PKI for the Int CA"
+
+  #
+  # 
+  #
+  default_lease_ttl_seconds = local.ONE_YEAR
+  max_lease_ttl_seconds = local.TWO_YEARS
+}
+
+resource "vault_pki_secret_backend_intermediate_cert_request" "IntermediateCSR" {
+  depends_on = [
+    vault_mount.RootPKI, vault_mount.PKI, time_sleep.RootPKIWait
+  ]
+
+  backend = vault_mount.PKI.path
+
+  type = "internal"
+  common_name = "pki-ca-int"
+
+  #
+  # Formats
+  #  
+  
+  #
+  # Vault Docs: https://www.vaultproject.io/api/secret/pki#format-2
+  # 
+  format = "pem"
+
+  #
+  # Vault Docs: https://www.vaultproject.io/api/secret/pki#private_key_format-2
+  #
+  private_key_format = "der"
+  
+  #
+  # Vault Docs: https://www.vaultproject.io/api/secret/pki#key_type-2
+  #
+  key_type = "ec"
+  
+  #
+  # Vault Docs: https://www.vaultproject.io/api/secret/pki#key_bits-2
+  #
+  key_bits = "384"
+}
+
+resource "vault_pki_secret_backend_root_sign_intermediate" "IntermediateCSRRootSign" {
+  depends_on = [
+    vault_pki_secret_backend_intermediate_cert_request.IntermediateCSR 
+  ]
+
+  backend = vault_mount.RootPKI.path
+
+  csr = vault_pki_secret_backend_intermediate_cert_request.IntermediateCSR.csr
+
+  common_name = "pki-ca-int"
+
+  exclude_cn_from_sans = true
+
+  organization = "kristianjones.dev"
+
+
+  #
+  # TTL
+  #  
+  ttl = local.TWO_YEARS
+}
+
+resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" { 
+  backend = vault_mount.PKI.path 
+  
+  certificate = "${vault_pki_secret_backend_root_sign_intermediate.IntermediateCSRRootSign.certificate}\n${vault_pki_secret_backend_root_cert.RootCA.certificate}"
+}
+
+resource "random_string" "RoleName" {
+  length           = 10
+
+  special = false
+  upper = true
+}
+
+resource "vault_pki_secret_backend_role" "PKIRole" {
+  depends_on = [
+    vault_mount.PKI,
+    vault_pki_secret_backend_intermediate_set_signed.intermediate
+  ]
+
+  backend = vault_mount.PKI.path
+
+  name    = random_string.RoleName.result
+
+  #
+  # Options
+  #
+  generate_lease = true
+  allow_any_name = true
+
+  #
+  # TTL
+  #
+  ttl = local.ONE_YEAR
+  max_ttl = local.ONE_YEAR
+
+  #
+  # Vault Options: https://www.vaultproject.io/api/secret/pki#key_usage
+  # Spec Options: https://pkg.go.dev/crypto/x509#KeyUsage
+  # Authenik Orig: https://registry.terraform.io/providers/goauthentik/authentik/latest/docs/resources/certificate_key_pair#example-usage
+  #
+  key_usage = ["KeyEncipherment", "DigitalSignature"]
+
+  #
+  # Vault Options: https://www.vaultproject.io/api/secret/pki#ext_key_usage
+  # Spec Options: https://pkg.go.dev/crypto/x509#ExtKeyUsage
+  # Authenik Orig: https://registry.terraform.io/providers/goauthentik/authentik/latest/docs/resources/certificate_key_pair#example-usage
+  ##
+  ext_key_usage = ["ServerAuth"]
 }
